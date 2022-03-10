b3ca348060c548dd800e75eacb4605d2e7b179ca
Joshua Postel <jpostel@umich.edu>
2022-03-10 14:03:18 -0500

    working executable

--- day1/src/day1.h
+++ day1/src/day1.h
@@ -1,4 +1,6 @@
 #include <fstream>
 
+// TODO how to namespace?
+
 int increase_count(std::fstream&);
 int windowed_increase_count(std::fstream&);
---
+++ day1/src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include "day1.h"
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cout << "usage: ./results my_filename.txt\n";
+  } else {
+    
+    std::fstream part1_input_file(argv[1]);
+    int part1_answer = increase_count(part1_input_file);
+    std::cout << "day1 part1 solution: " << part1_answer << "\n";
+  
+    std::fstream part2_input_file(argv[1]);
+    int part2_answer = windowed_increase_count(part2_input_file);
+    std::cout << "day1 part2 solution: " << part2_answer << "\n";
+
+  }
+
+}
