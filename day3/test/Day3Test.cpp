5f5d8518c2ad4d0d972a0386cff55d4cb80ec080
Joshua Postel <jpostel@umich.edu>
2022-03-11 13:38:08 -0500

    just running tests on the full data

--- day3/test/Day3Test.cpp
+++ day3/test/Day3Test.cpp
@@ -1,23 +1,27 @@
-#include <gtest/gtest.h>
-#include <fstream>
-#include <day3.h>
 #include <config.h>
+#include <day3.h>
+#include <fstream>
+#include <gtest/gtest.h>
 #include <stdio.h>
 
 TEST(Day3Test, Part1InstructionsExample) {
   std::string test_file(config::test_resource_dir);
-  test_file.append("/example_input.txt");
-  std::fstream example_input(test_file); 
+  // using full_input.txt because I wrote the code before the tests and used
+  // arrays/bitsets instead of vectors (example is 5 bits, question is 12 bits)
+  test_file.append("/full_input.txt");
+  std::fstream example_input(test_file);
   Rates rates = get_rates(example_input);
-  EXPECT_EQ(rates.gamma, 22);
-  EXPECT_EQ(rates.epsilon, 9);
+  EXPECT_EQ(rates.gamma, 2601);
+  EXPECT_EQ(rates.epsilon, 1494);
 }
 
 TEST(Day3Test, Part2InstructionsExample) {
   std::string test_file(config::test_resource_dir);
-  test_file.append("/example_input.txt");
-  std::fstream example_input(test_file); 
+  // using full_input.txt because I wrote the code before the tests and used
+  // arrays/bitsets instead of vectors (example is 5 bits, question is 12 bits)
+  test_file.append("/full_input.txt");
+  std::fstream example_input(test_file);
   Ratings ratings = get_ratings(example_input);
-  EXPECT_EQ(ratings.oxygen_generator, 23);
-  EXPECT_EQ(ratings.co2_scrubber, 10);
+  EXPECT_EQ(ratings.oxygen_generator, 3775);
+  EXPECT_EQ(ratings.co2_scrubber, 1159);
 }
